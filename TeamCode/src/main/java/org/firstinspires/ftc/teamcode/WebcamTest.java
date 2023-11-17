2e7b5cb9649a6ed136eef25c2ed51a397a628e1a
brian <griffinrobotics11666@gmail.com>
2023-11-17 09:39:45 -0800

    Added WebcamTest file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamTest.java
@@ -0,0 +1,125 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+import org.openftc.easyopencv.PipelineRecordingParameters;
+
+@TeleOp
+public class WebcamTest extends LinearOpMode
+{
+    OpenCvWebcam webcam;
+    @Override
+    public void runOpMode()
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        webcam.setPipeline(new org.firstinspires.ftc.teamcode.Pipelines.ContoursPixelLocatorRED(telemetry));
+        webcam.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
+
+            if(gamepad1.a)
+            {
+                webcam.stopStreaming();
+                //webcam.closeCameraDevice();
+            }
+
+            sleep(100);
+        }
+    }
+
+    class SamplePipeline extends OpenCvPipeline
+    {
+        boolean toggleRecording = false;
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            /*
+             * Draw a simple box around the middle 1/2 of the entire frame
+             */
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            input.cols()/4,
+                            input.rows()/4),
+                    new Point(
+                            input.cols()*(3f/4f),
+                            input.rows()*(3f/4f)),
+                    new Scalar(0, 255, 0), 4);
+
+            return input;
+        }
+
+        @Override
+        public void onViewportTapped()
+        {
+            toggleRecording = !toggleRecording;
+
+            if(toggleRecording)
+            {
+                /*
+                 * This is all you need to do to start recording.
+                 */
+                webcam.startRecordingPipeline(
+                        new PipelineRecordingParameters.Builder()
+                                .setBitrate(4, PipelineRecordingParameters.BitrateUnits.Mbps)
+                                .setEncoder(PipelineRecordingParameters.Encoder.H264)
+                                .setOutputFormat(PipelineRecordingParameters.OutputFormat.MPEG_4)
+                                .setFrameRate(30)
+                                .setPath("/sdcard/pipeline_rec.mp4")
+                                .build());
+            }
+            else
+            {
+                /*
+                 * Note: if you don't stop recording by yourself, it will be automatically
+                 * stopped for you at the end of your OpMode
+                 */
+                webcam.stopRecordingPipeline();
+            }
+        }
+    }
+}
